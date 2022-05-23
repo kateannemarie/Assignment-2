abcfd70a01337db831604c0f0b88ca70586e27e7
kateannemarie <kitkatfdalangin@gmail.com>
2022-05-23 14:20:20 +0800

    First commit

---
+++ TicTacToe.Dalangin/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace TicTacToe.Dalangin
+{
+    partial class TicTacToeLabel
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TicTacToeLabel));
+            this.ButtonA1 = new System.Windows.Forms.Button();
+            this.ButtonA2 = new System.Windows.Forms.Button();
+            this.ButtonA3 = new System.Windows.Forms.Button();
+            this.ButtonB1 = new System.Windows.Forms.Button();
+            this.ButtonB2 = new System.Windows.Forms.Button();
+            this.ButtonB3 = new System.Windows.Forms.Button();
+            this.ButtonC1 = new System.Windows.Forms.Button();
+            this.ButtonC2 = new System.Windows.Forms.Button();
+            this.ButtonC3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ButtonA1
+            // 
+            this.ButtonA1.Location = new System.Drawing.Point(34, 92);
+            this.ButtonA1.Name = "ButtonA1";
+            this.ButtonA1.Size = new System.Drawing.Size(85, 83);
+            this.ButtonA1.TabIndex = 0;
+            this.ButtonA1.UseVisualStyleBackColor = true;
+            // 
+            // ButtonA2
+            // 
+            this.ButtonA2.Location = new System.Drawing.Point(125, 92);
+            this.ButtonA2.Name = "ButtonA2";
+            this.ButtonA2.Size = new System.Drawing.Size(85, 83);
+            this.ButtonA2.TabIndex = 1;
+            this.ButtonA2.UseVisualStyleBackColor = true;
+            // 
+            // ButtonA3
+            // 
+            this.ButtonA3.Location = new System.Drawing.Point(216, 92);
+            this.ButtonA3.Name = "ButtonA3";
+            this.ButtonA3.Size = new System.Drawing.Size(85, 83);
+            this.ButtonA3.TabIndex = 2;
+            this.ButtonA3.UseVisualStyleBackColor = true;
+            // 
+            // ButtonB1
+            // 
+            this.ButtonB1.Location = new System.Drawing.Point(34, 181);
+            this.ButtonB1.Name = "ButtonB1";
+            this.ButtonB1.Size = new System.Drawing.Size(85, 83);
+            this.ButtonB1.TabIndex = 3;
+            this.ButtonB1.UseVisualStyleBackColor = true;
+            // 
+            // ButtonB2
+            // 
+            this.ButtonB2.Location = new System.Drawing.Point(125, 181);
+            this.ButtonB2.Name = "ButtonB2";
+            this.ButtonB2.Size = new System.Drawing.Size(85, 83);
+            this.ButtonB2.TabIndex = 4;
+            this.ButtonB2.UseVisualStyleBackColor = true;
+            // 
+            // ButtonB3
+            // 
+            this.ButtonB3.Location = new System.Drawing.Point(216, 181);
+            this.ButtonB3.Name = "ButtonB3";
+            this.ButtonB3.Size = new System.Drawing.Size(85, 83);
+            this.ButtonB3.TabIndex = 5;
+            this.ButtonB3.UseVisualStyleBackColor = true;
+            // 
+            // ButtonC1
+            // 
+            this.ButtonC1.Location = new System.Drawing.Point(34, 270);
+            this.ButtonC1.Name = "ButtonC1";
+            this.ButtonC1.Size = new System.Drawing.Size(85, 83);
+            this.ButtonC1.TabIndex = 6;
+            this.ButtonC1.UseVisualStyleBackColor = true;
+            // 
+            // ButtonC2
+            // 
+            this.ButtonC2.Location = new System.Drawing.Point(125, 270);
+            this.ButtonC2.Name = "ButtonC2";
+            this.ButtonC2.Size = new System.Drawing.Size(85, 83);
+            this.ButtonC2.TabIndex = 7;
+            this.ButtonC2.UseVisualStyleBackColor = true;
+            // 
+            // ButtonC3
+            // 
+            this.ButtonC3.Location = new System.Drawing.Point(216, 270);
+            this.ButtonC3.Name = "ButtonC3";
+            this.ButtonC3.Size = new System.Drawing.Size(85, 83);
+            this.ButtonC3.TabIndex = 8;
+            this.ButtonC3.UseVisualStyleBackColor = true;
+            // 
+            // TicTacToeLabel
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(335, 382);
+            this.Controls.Add(this.ButtonC3);
+            this.Controls.Add(this.ButtonC2);
+            this.Controls.Add(this.ButtonC1);
+            this.Controls.Add(this.ButtonB3);
+            this.Controls.Add(this.ButtonB2);
+            this.Controls.Add(this.ButtonB1);
+            this.Controls.Add(this.ButtonA3);
+            this.Controls.Add(this.ButtonA2);
+            this.Controls.Add(this.ButtonA1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(351, 421);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(351, 421);
+            this.Name = "TicTacToeLabel";
+            this.Text = "Tic Tac Toe";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ButtonA1;
+        private System.Windows.Forms.Button ButtonA2;
+        private System.Windows.Forms.Button ButtonA3;
+        private System.Windows.Forms.Button ButtonB1;
+        private System.Windows.Forms.Button ButtonB2;
+        private System.Windows.Forms.Button ButtonB3;
+        private System.Windows.Forms.Button ButtonC1;
+        private System.Windows.Forms.Button ButtonC2;
+        private System.Windows.Forms.Button ButtonC3;
+    }
+}
+
---
+++ TicTacToe.Dalangin/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TicTacToe.Dalangin
+{
+    public partial class TicTacToeLabel : Form
+    {
+        public TicTacToeLabel()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
