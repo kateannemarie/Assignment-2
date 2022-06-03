caf548df898967cab09ad2642a0415252f621b76
Kate Anne Marie Dalangin <104249599+kateannemarie@users.noreply.github.com>
2022-06-03 08:37:42 +0800

    Fixed errors

--- TicTacToe.Dalangin/Form1.Designer.cs
+++ TicTacToe.Dalangin/Form1.Designer.cs
@@ -39,123 +39,133 @@
             this.ButtonC2 = new System.Windows.Forms.Button();
             this.ButtonC3 = new System.Windows.Forms.Button();
             this.playerInstructions = new System.Windows.Forms.Button();
+            this.newgameButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // ButtonA1
             // 
+            this.ButtonA1.BackColor = System.Drawing.Color.Silver;
             this.ButtonA1.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.ButtonA1.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.ButtonA1.ForeColor = System.Drawing.Color.DimGray;
             this.ButtonA1.Location = new System.Drawing.Point(12, 55);
             this.ButtonA1.MaximumSize = new System.Drawing.Size(85, 83);
             this.ButtonA1.MinimumSize = new System.Drawing.Size(85, 83);
             this.ButtonA1.Name = "ButtonA1";
             this.ButtonA1.Size = new System.Drawing.Size(85, 83);
             this.ButtonA1.TabIndex = 0;
-            this.ButtonA1.UseVisualStyleBackColor = true;
+            this.ButtonA1.UseVisualStyleBackColor = false;
             this.ButtonA1.Click += new System.EventHandler(this.ButtonA1_Click);
             // 
             // ButtonA2
             // 
+            this.ButtonA2.BackColor = System.Drawing.Color.Silver;
             this.ButtonA2.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.ButtonA2.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.ButtonA2.ForeColor = System.Drawing.Color.DimGray;
             this.ButtonA2.Location = new System.Drawing.Point(103, 55);
             this.ButtonA2.MaximumSize = new System.Drawing.Size(85, 83);
             this.ButtonA2.MinimumSize = new System.Drawing.Size(85, 83);
             this.ButtonA2.Name = "ButtonA2";
             this.ButtonA2.Size = new System.Drawing.Size(85, 83);
             this.ButtonA2.TabIndex = 1;
-            this.ButtonA2.UseVisualStyleBackColor = true;
+            this.ButtonA2.UseVisualStyleBackColor = false;
             this.ButtonA2.Click += new System.EventHandler(this.ButtonA2_Click);
             // 
             // ButtonA3
             // 
+            this.ButtonA3.BackColor = System.Drawing.Color.Silver;
             this.ButtonA3.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.ButtonA3.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.ButtonA3.ForeColor = System.Drawing.Color.DimGray;
             this.ButtonA3.Location = new System.Drawing.Point(194, 55);
             this.ButtonA3.MaximumSize = new System.Drawing.Size(85, 83);
             this.ButtonA3.MinimumSize = new System.Drawing.Size(85, 83);
             this.ButtonA3.Name = "ButtonA3";
             this.ButtonA3.Size = new System.Drawing.Size(85, 83);
             this.ButtonA3.TabIndex = 2;
-            this.ButtonA3.UseVisualStyleBackColor = true;
+            this.ButtonA3.UseVisualStyleBackColor = false;
             this.ButtonA3.Click += new System.EventHandler(this.ButtonA3_Click);
             // 
             // ButtonB1
             // 
+            this.ButtonB1.BackColor = System.Drawing.Color.Silver;
             this.ButtonB1.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.ButtonB1.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.ButtonB1.ForeColor = System.Drawing.Color.DimGray;
             this.ButtonB1.Location = new System.Drawing.Point(12, 144);
             this.ButtonB1.MaximumSize = new System.Drawing.Size(85, 83);
             this.ButtonB1.MinimumSize = new System.Drawing.Size(85, 83);
             this.ButtonB1.Name = "ButtonB1";
             this.ButtonB1.Size = new System.Drawing.Size(85, 83);
             this.ButtonB1.TabIndex = 3;
-            this.ButtonB1.UseVisualStyleBackColor = true;
+            this.ButtonB1.UseVisualStyleBackColor = false;
             this.ButtonB1.Click += new System.EventHandler(this.ButtonB1_Click);
             // 
             // ButtonB2
             // 
+            this.ButtonB2.BackColor = System.Drawing.Color.Silver;
             this.ButtonB2.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.ButtonB2.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.ButtonB2.ForeColor = System.Drawing.Color.DimGray;
             this.ButtonB2.Location = new System.Drawing.Point(103, 144);
             this.ButtonB2.MaximumSize = new System.Drawing.Size(85, 83);
             this.ButtonB2.MinimumSize = new System.Drawing.Size(85, 83);
             this.ButtonB2.Name = "ButtonB2";
             this.ButtonB2.Size = new System.Drawing.Size(85, 83);
             this.ButtonB2.TabIndex = 4;
-            this.ButtonB2.UseVisualStyleBackColor = true;
+            this.ButtonB2.UseVisualStyleBackColor = false;
             this.ButtonB2.Click += new System.EventHandler(this.ButtonB2_Click);
             // 
             // ButtonB3
             // 
+            this.ButtonB3.BackColor = System.Drawing.Color.Silver;
             this.ButtonB3.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.ButtonB3.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.ButtonB3.ForeColor = System.Drawing.Color.DimGray;
             this.ButtonB3.Location = new System.Drawing.Point(194, 144);
             this.ButtonB3.MaximumSize = new System.Drawing.Size(85, 83);
             this.ButtonB3.MinimumSize = new System.Drawing.Size(85, 83);
             this.ButtonB3.Name = "ButtonB3";
             this.ButtonB3.Size = new System.Drawing.Size(85, 83);
             this.ButtonB3.TabIndex = 5;
-            this.ButtonB3.UseVisualStyleBackColor = true;
+            this.ButtonB3.UseVisualStyleBackColor = false;
             this.ButtonB3.Click += new System.EventHandler(this.ButtonB3_Click);
             // 
             // ButtonC1
             // 
+            this.ButtonC1.BackColor = System.Drawing.Color.Silver;
             this.ButtonC1.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.ButtonC1.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.ButtonC1.ForeColor = System.Drawing.Color.DimGray;
             this.ButtonC1.Location = new System.Drawing.Point(12, 233);
             this.ButtonC1.MaximumSize = new System.Drawing.Size(85, 83);
             this.ButtonC1.MinimumSize = new System.Drawing.Size(85, 83);
             this.ButtonC1.Name = "ButtonC1";
             this.ButtonC1.Size = new System.Drawing.Size(85, 83);
             this.ButtonC1.TabIndex = 6;
-            this.ButtonC1.UseVisualStyleBackColor = true;
+            this.ButtonC1.UseVisualStyleBackColor = false;
             this.ButtonC1.Click += new System.EventHandler(this.ButtonC1_Click);
             // 
             // ButtonC2
             // 
+            this.ButtonC2.BackColor = System.Drawing.Color.Silver;
             this.ButtonC2.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.ButtonC2.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.ButtonC2.ForeColor = System.Drawing.Color.DimGray;
             this.ButtonC2.Location = new System.Drawing.Point(103, 233);
             this.ButtonC2.MaximumSize = new System.Drawing.Size(85, 83);
             this.ButtonC2.MinimumSize = new System.Drawing.Size(85, 83);
             this.ButtonC2.Name = "ButtonC2";
             this.ButtonC2.Size = new System.Drawing.Size(85, 83);
             this.ButtonC2.TabIndex = 7;
-            this.ButtonC2.UseVisualStyleBackColor = true;
+            this.ButtonC2.UseVisualStyleBackColor = false;
             this.ButtonC2.Click += new System.EventHandler(this.ButtonC2_Click);
             // 
             // ButtonC3
             // 
+            this.ButtonC3.BackColor = System.Drawing.Color.Silver;
             this.ButtonC3.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.ButtonC3.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.ButtonC3.ForeColor = System.Drawing.Color.DimGray;
             this.ButtonC3.Location = new System.Drawing.Point(194, 233);
             this.ButtonC3.MaximumSize = new System.Drawing.Size(85, 83);
             this.ButtonC3.MinimumSize = new System.Drawing.Size(85, 83);
             this.ButtonC3.Name = "ButtonC3";
             this.ButtonC3.Size = new System.Drawing.Size(85, 83);
             this.ButtonC3.TabIndex = 8;
-            this.ButtonC3.UseVisualStyleBackColor = true;
+            this.ButtonC3.UseVisualStyleBackColor = false;
             this.ButtonC3.Click += new System.EventHandler(this.ButtonC3_Click);
             // 
             // playerInstructions
@@ -172,11 +182,28 @@
             this.playerInstructions.UseVisualStyleBackColor = true;
             this.playerInstructions.Click += new System.EventHandler(this.playerInstructions_Click);
             // 
+            // newgameButton
+            // 
+            this.newgameButton.Font = new System.Drawing.Font("Yu Gothic UI", 6.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.newgameButton.ForeColor = System.Drawing.Color.Red;
+            this.newgameButton.Location = new System.Drawing.Point(204, 12);
+            this.newgameButton.MaximumSize = new System.Drawing.Size(75, 23);
+            this.newgameButton.MinimumSize = new System.Drawing.Size(75, 23);
+            this.newgameButton.Name = "newgameButton";
+            this.newgameButton.Size = new System.Drawing.Size(75, 23);
+            this.newgameButton.TabIndex = 10;
+            this.newgameButton.Text = "New Game";
+            this.newgameButton.UseVisualStyleBackColor = true;
+            this.newgameButton.Click += new System.EventHandler(this.button1_Click);
+            // 
             // TicTacToeLabel
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::TicTacToe.Dalangin.Properties.Resources.istockphoto_1160542809_612x612;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(291, 331);
+            this.Controls.Add(this.newgameButton);
             this.Controls.Add(this.playerInstructions);
             this.Controls.Add(this.ButtonC3);
             this.Controls.Add(this.ButtonC2);
@@ -211,6 +238,7 @@
         private System.Windows.Forms.Button ButtonC2;
         private System.Windows.Forms.Button ButtonC3;
         private System.Windows.Forms.Button playerInstructions;
+        private System.Windows.Forms.Button newgameButton;
     }
 }
 
--- TicTacToe.Dalangin/Form1.cs
+++ TicTacToe.Dalangin/Form1.cs
@@ -22,7 +22,7 @@ namespace TicTacToe.Dalangin
 
         public String backSymbol(int turn)
         {
-            if(turn % 2 == 0)
+            if (turn % 2 == 0)
             {
                 return "O";
             }
@@ -32,48 +32,90 @@ namespace TicTacToe.Dalangin
             }
         }
 
+        public System.Drawing.Color colorChanger(String symbol)
+        {
+            if (symbol.Equals("O"))
+            {
+                return System.Drawing.Color.LightYellow;
+            }
+            else if (symbol.Equals("X"))
+            {
+                return System.Drawing.Color.LightBlue;
+            }
+            return System.Drawing.Color.Silver;
+        }
+
         public void theWinner()
         {
             for(int i=0; i<8; i++)
             {
                 String combination = "";
+                int one = 0, two = 0, three = 0;
 
-                switch(i)
+                switch (i)
                 {
                     case 0:
                         combination = keyPad[2] + keyPad[4] + keyPad[6];
+                        one = 2;
+                        two = 4;
+                        three = 6;
                         break;
                     case 1:
                         combination = keyPad[0] + keyPad[4] + keyPad[8];
+                        one = 0;
+                        two = 4;
+                        three = 8;
                         break;
                     case 2:
                         combination = keyPad[0] + keyPad[1] + keyPad[2];
+                        one = 0;
+                        two = 1;
+                        three = 2;
                         break;
                     case 3:
                         combination = keyPad[3] + keyPad[4] + keyPad[5];
+                        one = 3;
+                        two = 4;
+                        three = 5;
                         break;
                     case 4:
                         combination = keyPad[6] + keyPad[7] + keyPad[8];
+                        one = 6;
+                        two = 7;
+                        three = 8;
                         break;
                     case 5:
                         combination = keyPad[2] + keyPad[5] + keyPad[8];
+                        one = 2;
+                        two = 5;
+                        three = 8;
                         break;
                     case 6:
                         combination = keyPad[1] + keyPad[4] + keyPad[7];
+                        one = 1;
+                        two = 4;
+                        three = 7;
                         break;
                     case 7:
                         combination = keyPad[0] + keyPad[3] + keyPad[6];
+                        one = 0;
+                        two = 3;
+                        three = 6;
                         break;
                 }
 
                 if(combination.Equals("OOO"))
                 {
-                    reset();
+                    winnerColor(one);
+                    winnerColor(two);
+                    winnerColor(three);
                     MessageBox.Show("Player O has won the game!", "We have a winner!", MessageBoxButtons.OK);
                 }
                 else if(combination.Equals("XXX"))
                 {
-                    reset();
+                    winnerColor(one);
+                    winnerColor(two);
+                    winnerColor(three);
                     MessageBox.Show("Player X has won the game!", "We have a winner!", MessageBoxButtons.OK);
                 }
 
@@ -92,11 +134,47 @@ namespace TicTacToe.Dalangin
             ButtonC1.Text = "";
             ButtonC2.Text = "";
             ButtonC3.Text = "";
+            ButtonA1.BackColor = System.Drawing.Color.Silver;
+            ButtonA2.BackColor = System.Drawing.Color.Silver;
+            ButtonA3.BackColor = System.Drawing.Color.Silver;
+            ButtonB1.BackColor = System.Drawing.Color.Silver;
+            ButtonB2.BackColor = System.Drawing.Color.Silver;
+            ButtonB3.BackColor = System.Drawing.Color.Silver;
+            ButtonC1.BackColor = System.Drawing.Color.Silver;
+            ButtonC2.BackColor = System.Drawing.Color.Silver;
+            ButtonC3.BackColor = System.Drawing.Color.Silver;
             keyPad = new string[9];
             currentPlayer = 0;
         }
 
+        public void winnerColor(int number)
+        {
+            switch (number)
+            {
+                case 0:
+                    ButtonA1.BackColor = System.Drawing.Color.Green; break;
+                case 1:
+                    ButtonA2.BackColor = System.Drawing.Color.Green; break;
+                case 2:
+                    ButtonA3.BackColor = System.Drawing.Color.Green; break;
+                case 3:
+                    ButtonB1.BackColor = System.Drawing.Color.Green; break;
+                case 4:
+                    ButtonB2.BackColor = System.Drawing.Color.Green; break;
+                case 5:
+                    ButtonB3.BackColor = System.Drawing.Color.Green; break;
+                case 6:
+                    ButtonC1.BackColor = System.Drawing.Color.Green; break;
+                case 7:
+                    ButtonC2.BackColor = System.Drawing.Color.Green; break;
+                case 8:
+                    ButtonC3.BackColor = System.Drawing.Color.Green; break;
+
+            }
+        }
+
         public void tieBreaker()
+            
         {
             int counter = 0;
             for(int i=0; i<keyPad.Length; i++)
@@ -120,6 +198,8 @@ namespace TicTacToe.Dalangin
         {
             currentPlayer++;
             keyPad[0] = backSymbol(currentPlayer);
+            Color keypadColor = colorChanger(keyPad[0]);
+            ButtonA1.BackColor = keypadColor;
             ButtonA1.Text = keyPad[0];
             theWinner();
         }
@@ -128,6 +208,8 @@ namespace TicTacToe.Dalangin
         {
             currentPlayer++;
             keyPad[1] = backSymbol(currentPlayer);
+            Color keypadColor = colorChanger(keyPad[1]);
+            ButtonA2.BackColor = keypadColor;
             ButtonA2.Text = keyPad[1];
             theWinner();
         }
@@ -136,6 +218,8 @@ namespace TicTacToe.Dalangin
         {
             currentPlayer++;
             keyPad[2] = backSymbol(currentPlayer);
+            Color keypadColor = colorChanger(keyPad[2]);
+            ButtonA3.BackColor = keypadColor;
             ButtonA3.Text = keyPad[2];
             theWinner();
         }
@@ -144,6 +228,8 @@ namespace TicTacToe.Dalangin
         {
             currentPlayer++;
             keyPad[3] = backSymbol(currentPlayer);
+            Color keypadColor = colorChanger(keyPad[3]);
+            ButtonB1.BackColor = keypadColor;
             ButtonB1.Text = keyPad[3];
             theWinner();
         }
@@ -152,6 +238,8 @@ namespace TicTacToe.Dalangin
         {
             currentPlayer++;
             keyPad[4] = backSymbol(currentPlayer);
+            Color keypadColor = colorChanger(keyPad[4]);
+            ButtonB2.BackColor = keypadColor;
             ButtonB2.Text = keyPad[4];
             theWinner();
         }
@@ -160,6 +248,8 @@ namespace TicTacToe.Dalangin
         {
             currentPlayer++;
             keyPad[5] = backSymbol(currentPlayer);
+            Color keypadColor = colorChanger(keyPad[5]);
+            ButtonB3.BackColor = keypadColor;
             ButtonB3.Text = keyPad[5];
             theWinner();
         }
@@ -168,6 +258,8 @@ namespace TicTacToe.Dalangin
         {
             currentPlayer++;
             keyPad[6] = backSymbol(currentPlayer);
+            Color keypadColor = colorChanger(keyPad[6]);
+            ButtonC1.BackColor = keypadColor;
             ButtonC1.Text = keyPad[6];
             theWinner();
         }
@@ -176,6 +268,8 @@ namespace TicTacToe.Dalangin
         {
             currentPlayer++;
             keyPad[7] = backSymbol(currentPlayer);
+            Color keypadColor = colorChanger(keyPad[7]);
+            ButtonC2.BackColor = keypadColor;
             ButtonC2.Text = keyPad[7];
             theWinner();
         }
@@ -184,13 +278,20 @@ namespace TicTacToe.Dalangin
         {
             currentPlayer++;
             keyPad[8] = backSymbol(currentPlayer);
+            Color keypadColor = colorChanger(keyPad[8]);
+            ButtonC3.BackColor = keypadColor;
             ButtonC3.Text = keyPad[8];
             theWinner();
         }
 
         private void playerInstructions_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("The object of Tic Tac Toe is to get three in a row. You play on a three by three game board. The first player is known as X and the second is O. Players alternate placing Xs and Os on the game board until either opponent has three in a row or all nine squares are filled. X always goes first, and in the event that no one has three in a row, the stalemate is called a tie game.");
+            MessageBox.Show("The object of Tic Tac Toe is to get three in a row. You play on a three by three game board. The first player is known as X and the second is O. Players alternate placing Xs and Os on the game board until either opponent has three in a row or all nine squares are filled. X always goes first, and in the event that no one has three in a row, the stalemate is called a tie game.", "Instructions", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            reset();
         }
     }
 }
